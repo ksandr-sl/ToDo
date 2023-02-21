2e2241c62c0e1b0709c6abbeebf52ecefc8380b1
ksandrsl <ksandr.sl@gmail.com>
2023-02-21 14:47:48 +0600

    fixed image paths

--- js/index.js
+++ js/index.js
@@ -82,7 +82,7 @@ function doneTask(event) {
     // находим задачу в массиве
     const task = tasks.find(task => task.id === id);
 
-    !task.done ? doneImg.src = '/img/svg/back.svg': doneImg.src = '/img/svg/green-check.svg';
+    !task.done ? doneImg.src = 'img/svg/back.svg': doneImg.src = 'img/svg/green-check.svg';
 
     task.done = !task.done;
 
@@ -95,7 +95,7 @@ function doneTask(event) {
 function checkEmptyList() {
     const emptyListHTML = `
     <li id="emptylist" class="body__emptylist">
-        <img src="/img/empty.png" alt="" class="body__emptyImg"/>
+        <img src="img/empty.png" alt="" class="body__emptyImg"/>
         <div>No tasks for today</div>
     </li>`;
 
@@ -114,7 +114,7 @@ function saveToLocalStorage() {
 
 function renderTask(task) {
     const taskCssClass = task.done ? 'body__task --done' : 'body__task';
-    const srcImg = task.done ? '/img/svg/back.svg' : '/img/svg/green-check.svg';
+    const srcImg = task.done ? 'img/svg/back.svg' : 'img/svg/green-check.svg';
     const textCssClass = task.length > 56 ? 'body__task-title --padding' : 'body__task-title';
 
     // формируем разметку для новой задачи
@@ -126,7 +126,7 @@ function renderTask(task) {
                 <img src="${srcImg}" alt="" class="body__done-img">
             </button>
             <button data-action="delete" type="button" class="body__btn-delete">
-                <img src="/img/svg/delete-img.svg" alt="" class="body__delete-img">
+                <img src="img/svg/delete-img.svg" alt="" class="body__delete-img">
             </button>
         </div>
     </li>`;
